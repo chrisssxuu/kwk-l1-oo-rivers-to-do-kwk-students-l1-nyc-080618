1a5d01ef9fc4e32d588e25318d202771a7644da6
chrisssxuu <chris01.xu@gmail.com>
2018-08-10 14:52:32 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -3,9 +3,15 @@ class River
   def initialize(name)
     @name = name
   end
+  
   def name
     @name
   end
+  
+  def name=(new_name)
+    @name=new_name
+  end
+  
   def length
     @length
   end
