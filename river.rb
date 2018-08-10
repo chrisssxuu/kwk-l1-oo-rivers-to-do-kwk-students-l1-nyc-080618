f3a1c26f7eec560915a3f7434df31a597e45de6c
chrisssxuu <chris01.xu@gmail.com>
2018-08-10 15:03:55 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -37,11 +37,11 @@ class River
   end
   
   def flood
-    @discharge = (@discharge * 1.3)
+    @discharge = @discharge * 1.3
   end 
   
   def dry_up
-    @discharge == (@discharge * 0.5)
+    @discharge = @discharge * 0.5
   end
 end
 
